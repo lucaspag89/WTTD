8dc996225199e2e35b513860f659997e1bfae316
Lucas Pagliarini <lucas_pagliarini@hotmail.com>
2023-02-07 23:15:13 -0300

    subscriptions admin

--- eventex/settings.py
+++ eventex/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'test_without_migrations',
     'eventex.core',
-    'eventex.subscriptions',
+    'eventex.subscriptions.apps.SubscriptionsConfig',
 ]
 
 MIDDLEWARE = [
